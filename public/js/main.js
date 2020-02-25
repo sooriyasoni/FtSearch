ce45d6578e1c163e9076ff2a42e4e72315392d92
Sooryachal <sooryachal@Sooryachals-MacBook-Air.local>
2020-02-25 02:05:29 +0000

    make it better

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
-var url = 'https://ftsearch-v1.herokuapp.com/search';
-import fetch from 'isomorphic-unfetch';
+
+
 
 //make sure service worked are supported
 if ('serviceWorker' in navigator) {
@@ -12,13 +12,7 @@ if ('serviceWorker' in navigator) {
     });
 }
 //for search on enter key
-var input = document.getElementById("query");
-input.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("go").click();
-    }
-})
+
 
 function onLoad() {
     document.getElementById('prev').style.display = 'none';
@@ -36,53 +30,51 @@ function onClickPage(pageId) {
         aspects: aspects,
         maxResults: 10
     };
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            queryString: document.getElementById('query').value,
-            resultContext: resultContext
-        })
-    })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (myJson) {
-            console.log(myJson);
-            if (myJson.error == false) {
-                var res = myJson.data;
-                var txt = '';
-                txt = '<div class="search-results o-teaser-collection" ><div class="search-results__heading"><div class="search-results__heading-title"> <h2 class="o-teaser-collection__heading o-teaser-collection__heading--half-width"> Viewing Search Results for <strong>"' + document.getElementById('query').value + '"</strong> </h2> </div> </div>';
+    var http = new XMLHttpRequest();
+    // var url = 'https://ftsearch-v1.herokuapp.com/search';
+    var url = "http://localhost:5000/search"
+    var params = {
+        queryString: document.getElementById('query').value,
+        resultContext: resultContext
+    }
 
-                for (var i in res.results[0].results) {
-                    if (res.results[0].results != undefined) {
-                        var title = res.results[0].results[i].title;
-                        var apiUrl = res.results[0].results[i].apiUrl;
-                        var summary = res.results[0].results[i].summary.excerpt;
-                        txt += '<ul class="search-results__list" > <li class="search-results__list-item" > <div class="search-item"> <div class="search-item__teaser"> <div class="o-teaser o-teaser--article o-teaser--small o-teaser--has-image js-teaser" > <div class="o-teaser__content"><div class="o-teaser__heading"> <a href=' + apiUrl + '  class="js-teaser-heading-link">' + title.title + '</a> </div> <p class="o-teaser__standfirst"> <a href=' + apiUrl + ' class="js-teaser-standfirst-link"><span>' + summary + '</span> </a> </p> </div> </div> </div> </div> </li> </ul>';
-                    }
-                }
-                txt += '</div>';
-                document.getElementById('search-content').innerHTML = txt;
-                document.getElementById('pagination').style.display = 'block';
-                document.getElementById('prev').style.display = 'block';
-                document.getElementById('curr').style.display = 'block';
-                document.getElementById('next').style.display = 'block';
-                console.log(pageId);
-                console.log('pageNo', pageNo);
-                var currPage = pageNo + 1;
-                document.getElementById('prev').value = pageNo - 1;
-                document.getElementById('next').value = pageNo + 1;
-                document.getElementById('curr').innerHTML = "Page " + currPage;
-                if (pageNo == 0) {
-                    document.getElementById('prev').style.display = 'none';
+    http.open('POST', url, true);
+    //Send the proper header information along with the request
+    http.setRequestHeader('Content-type', 'application/json');
+
+    http.send(JSON.stringify(params)) // Make sure to stringify
+    http.onload = function () {
+        // Do whatever with response
+        var myJson = JSON.parse(this.responseText);
+        if (this.responseText.error != true) {
+            var res = myJson.data;
+            var txt = '';
+            txt = '<div class="search-results o-teaser-collection" ><div class="search-results__heading"><div class="search-results__heading-title"> <h2 class="o-teaser-collection__heading o-teaser-collection__heading--half-width"> Viewing Search Results for <strong>"' + document.getElementById('query').value + '"</strong> </h2> </div> </div>';
+
+            for (var i in res.results[0].results) {
+                if (res.results[0].results != undefined) {
+                    var title = res.results[0].results[i].title;
+                    var apiUrl = res.results[0].results[i].apiUrl;
+                    var summary = res.results[0].results[i].summary.excerpt;
+                    txt += '<ul class="search-results__list" > <li class="search-results__list-item" > <div class="search-item"> <div class="search-item__teaser"> <div class="o-teaser o-teaser--article o-teaser--small o-teaser--has-image js-teaser" > <div class="o-teaser__content"><div class="o-teaser__heading"> <a href=' + apiUrl + '  class="js-teaser-heading-link">' + title.title + '</a> </div> <p class="o-teaser__standfirst"> <a href=' + apiUrl + ' class="js-teaser-standfirst-link"><span>' + summary + '</span> </a> </p> </div> </div> </div> </div> </li> </ul>';
                 }
             }
-        })
-        .catch(function (err) {
-            console.log(err);
-            document.getElementById('demo').innerHTML = err;
-        });
+            txt += '</div>';
+            document.getElementById('search-content').innerHTML = txt;
+            document.getElementById('pagination').style.display = 'block';
+            document.getElementById('prev').style.display = 'block';
+            document.getElementById('curr').style.display = 'block';
+            document.getElementById('next').style.display = 'block';
+            // console.log(pageId);
+            // console.log('pageNo', pageNo);
+            var currPage = pageNo + 1;
+            document.getElementById('prev').value = pageNo - 1;
+            document.getElementById('next').value = pageNo + 1;
+            document.getElementById('curr').innerHTML = "Page " + currPage;
+            if (pageNo == 0) {
+                document.getElementById('prev').style.display = 'none';
+            }
+        }
+    }
 }
+
